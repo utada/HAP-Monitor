a8a65173b51fd3344808d2bcaf6c53336d083981
utada <utada4@gmail.com>
2016-07-17 13:51:45 +0900

    backend_statsd socket protocol UDP -> TCP

--- hap-monitor-cron.py
+++ hap-monitor-cron.py
@@ -97,14 +97,15 @@ def backend_graphite(url, stats, prefix):
     logger.debug('Reporting to prefix: %s' % prefix)
     server, port = url.split(':')
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((server, int(port)))
     except Exception, e:
         logger.error('Unable to connect to Graphite backend %s: %s' % (url, e))
         raise
 
     for s in stats:
         try:
-            sock.sendto('%s.%s %s %s\n' % (prefix, s, float(stats[s]), int(time.time())), (server, int(port)))
+            sock.send('%s.%s %s %s\n' % (prefix, s, float(stats[s]), int(time.time())))
             logger.debug('Message: %s.%s %s %s\n' % (prefix, s, float(stats[s]), int(time.time())))
         except Exception as e:
             logger.error('Failed reporting %s.%s %s %s\n' % (prefix, s, float(stats[s]), time.time()))
